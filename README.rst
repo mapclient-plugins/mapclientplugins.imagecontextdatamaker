74b35a7bec62aba8ac75317edf06df4bfc33e868
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-10-24 16:39:51 +1300

    Initial commit of skeleton code.

---
+++ README.rst
@@ -0,0 +1,5 @@
+Image Context Data Maker
+========================
+
+The Image Context Data Maker step is a plugin for the MAP Client application.
+
---
+++ mapclientplugins/imagecontextdatamakerstep/__init__.py
@@ -0,0 +1,16 @@
+
+"""
+MAP Client Plugin
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Hugh Sorby'
+__stepname__ = 'Image Context Data Maker'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.imagecontextdatamaker/archive/master.zip'
+
+# import class that derives itself from the step mountpoint.
+from mapclientplugins.imagecontextdatamakerstep import step
+
+# Import the resource file when the module is loaded,
+# this enables the framework to use the step icon.
+from . import resources_rc
